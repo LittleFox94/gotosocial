489f5dd7a02263e91039710307abc43081632270
Mara Sophie Grosch <littlefox@lf-net.org>
2023-08-11 18:49:08 +0200

    Content-Security-Policy + S3: fix img-src directive

--- internal/middleware/extraheaders.go
+++ internal/middleware/extraheaders.go
@@ -84,7 +84,7 @@ func BuildContentSecurityPolicy() string {
 	s3EndpointURLStr := scheme + "://" + s3Endpoint
 
 	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/img-src
-	policy += "; image-src " + s3EndpointURLStr
+	policy += "; img-src " + s3EndpointURLStr
 
 	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/media-src
 	policy += "; media-src " + s3EndpointURLStr
